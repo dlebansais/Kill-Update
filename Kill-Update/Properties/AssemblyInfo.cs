5ee94a86cda62267298d8ebb6db67b64153d4871
dlebansais <dlebansais@gmail.com>
2018-06-25 22:55:12 +0200

    Updated the version number.

--- Kill-Update/Properties/AssemblyInfo.cs
+++ Kill-Update/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.35")]
-[assembly: AssemblyFileVersion("1.0.0.34")]
+[assembly: AssemblyVersion("1.0.0.36")]
+[assembly: AssemblyFileVersion("1.0.0.35")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
