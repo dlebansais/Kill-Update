a1e5a6b8ea41078dde05d04074462b9746138524
dlebansais <dlebansais@gmail.com>
2019-05-27 18:46:50 +0200

    Small reorganization of dependencies.

--- Kill-Update-Plugin/Properties/AssemblyInfo.cs
+++ Kill-Update-Plugin/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.36")]
-[assembly: AssemblyFileVersion("1.0.0.36")]
+[assembly: AssemblyVersion("1.0.0.40")]
+[assembly: AssemblyFileVersion("1.0.0.39")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
