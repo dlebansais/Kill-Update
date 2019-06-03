4ca1d179faca11f14be24c9fb29c04cb9826fe81
dlebansais <dlebansais@gmail.com>
2019-06-03 13:42:23 +0200

    Updated the digital certificate.

--- SinglePluginHost/Properties/AssemblyInfo.cs
+++ SinglePluginHost/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.40")]
-[assembly: AssemblyFileVersion("1.0.0.40")]
+[assembly: AssemblyVersion("1.0.0.44")]
+[assembly: AssemblyFileVersion("1.0.0.45")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
